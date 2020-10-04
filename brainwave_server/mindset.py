552238f6908eadedf09ef0e28657385a10c54ae2
Nate8888 <wilk.nathan@gmail.com>
2020-10-03 22:46:16 -0400

    brainwave files, headset working

--- brainwave_server/mindset.py
+++ brainwave_server/mindset.py
@@ -2,60 +2,82 @@ from NeuroPyThree import NeuroPy
 import time
 import os
 import json
-
+from datetime import datetime
 
 object1=NeuroPy("COM4")
 
+allbrainwaves = {
+                 'att':[],
+                 'rlx':[],
+                 'del':[],
+                 'the':[],
+                 'lal':[],
+                 'hal':[],
+                 'hbe':[],
+                 'lbe':[],
+                 'lga':[],
+                 'mga':[]
+                 }
 
 def attention_callback(value):
     if value != 0:
+        allbrainwaves['att'].append(value)
         with open('att.txt', "w") as outfile:
             outfile.write(str(value))
 
 def meditation_callback(value):
     if value != 0:
+        allbrainwaves['rlx'].append(value)
         with open('rlx.txt', 'w') as outfile:
             outfile.write(str(value))
 
 def delta_waves_callback(value):
     if value != 0:
+        allbrainwaves['del'].append(value)
         with open('del.txt', 'w') as outfile:
             outfile.write(str(value))
 
 def theta_waves_callback(value):
     if value != 0:
+        allbrainwaves['the'].append(value)
         with open('the.txt', 'w') as outfile:
             outfile.write(str(value))
 
 def lowalpha_waves_callback(value):
     if value != 0:
+        allbrainwaves['lal'].append(value)
         with open('lal.txt', 'w') as outfile:
             outfile.write(str(value))
 
 def highalpha_waves_callback(value):
     if value != 0:
+        allbrainwaves['hal'].append(value)
         with open('hal.txt', 'w') as outfile:
             outfile.write(str(value))
 
 
 def lowbeta_waves_callback(value):
     if value != 0:
+        allbrainwaves['lbe'].append(value)
         with open('lbe.txt', 'w') as outfile:
             outfile.write(str(value))
 
 def highbeta_waves_callback(value):
     if value != 0:
+        allbrainwaves['hbe'].append(value)
         with open('hbe.txt', 'w') as outfile:
             outfile.write(str(value))
 
 
 def lowgamma_waves_callback(value):
     if value != 0:
+        allbrainwaves['lga'].append(value)
         with open('lga.txt', 'w') as outfile:
             outfile.write(str(value))
 
 def midgamma_waves_callback(value):
     if value != 0:
+        allbrainwaves['mga'].append(value)
         with open('mga.txt', 'w') as outfile:
             outfile.write(str(value))
 
@@ -77,7 +99,11 @@ object1.setCallBack("lowGamma",lowgamma_waves_callback)
 object1.setCallBack("midGamma",midgamma_waves_callback)
 
 object1.start()
-time.sleep(2)
+time.sleep(4)
+now = datetime.now().time()
 
 while True:
-    time.sleep(0.2)
+    time.sleep(2)
+    fin = open("complete_data.txt","w")
+    fin.write(json.dumps(allbrainwaves))
+    print(json.dumps(allbrainwaves))
